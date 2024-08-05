8a753f118ffe98331f65f0a9a77ba3ed8ad9936c
hayapy88 <payagram01@gmail.com>
2024-08-05 23:24:39 +1000

    Start the server and check the display

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("node.js");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Node.js");
+});
