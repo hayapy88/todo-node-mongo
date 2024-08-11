d2e82a260433bc595a3efbe90c0057777820c26e
hayapy88 <payagram01@gmail.com>
2024-08-12 03:24:22 +1000

    Add submit event and update list after submission

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+const todoTasksListEl = document.getElementById("todoTasksList");
+const formEl = document.getElementById("addTaskForm");
+const taskNameEl = document.getElementById("taskName");
+
 const displayTasks = async () => {
   try {
     const { data: tasks } = await axios.get("/api/v1/tasks");
@@ -19,10 +23,22 @@ const displayTasks = async () => {
       </div>`;
       })
       .join("");
-    const todoTasksListEl = document.getElementById("todoTasksList");
     todoTasksListEl.innerHTML = allTasks;
   } catch (err) {
     console.log(err);
   }
 };
 displayTasks();
+
+formEl.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const taskNameVal = taskNameEl.value;
+
+  try {
+    await axios.post("/api/v1/tasks", { name: taskNameVal });
+    displayTasks();
+    taskNameEl.value = "";
+  } catch (err) {
+    console.log(err);
+  }
+});
