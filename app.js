a9d451d3ce14a2988f4ae12596055947f36108b9
hayapy88 <payagram01@gmail.com>
2024-08-13 08:53:37 +1000

    Try fixing port issues

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ require("dotenv").config();
 app.use(express.json());
 app.use(express.static("./public"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/api/v1/tasks", taskRoute);
 
 const start = async () => {
   try {
     await connectDB(process.env.DATABASE_URL);
-    app.listen(
-      process.env.port || port,
-      console.log(`ToDo app listening on port ${port}`)
-    );
+    app.listen(port, console.log(`ToDo app listening on port ${port}`));
   } catch (err) {
     console.log(err);
   }
